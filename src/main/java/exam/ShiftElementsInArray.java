86a8e018ec7da5f1fba4193ff0fe8f8d357843ed
Sourav Vishnusing Pardeshi <83573696+SouravPardeshi@users.noreply.github.com>
2021-06-22 12:55:49 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -8,3 +8,32 @@ public class ShiftElementsInArray {
   }
 
 }
+public class Solution {
+    public static void LeftShift(int arr[], int n) {
+        int temp = arr[0];
+        for (int i = 0; i < n-1; i++) {
+            arr[i] = arr[i+1];
+        }       
+        arr[n-1] = temp;
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        
+        /* Left rotate for k times */
+        for (int i = 0; i < k; i++) {
+            LeftShift(a, n);
+        }
+        
+        /* Displaying the output */
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
