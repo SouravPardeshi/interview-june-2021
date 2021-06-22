4b31a84198b915b3f49cd2c90141250b2dfa0bf9
Sourav Vishnusing Pardeshi <83573696+SouravPardeshi@users.noreply.github.com>
2021-06-22 12:51:23 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -1,10 +1,89 @@
 package exam;
 
-public class CountCombinations {
+public class CountCombinations {	
+	public static void main(String[] args){
+		Object[] elements = new Object[] {1,2,3,4,5};
+		combination(elements,3);
+	}
+	
+	
+	public static void combination(Object[]  elements, int K){
 
-  public int getNumberOfWays(final int total, final int k) {
-    //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return 0;
-  }
+	
+		int N = elements.length;
+		
+		if(K > N){
+			System.out.println("Invalid input, K > N");
+			return;
+		}
+		
+		// calculate the possible combinations
+		c(N,K);
+		
+		// init combination index array
+		int combination[] = new int[K];
+		
 
+		int r = 0; // index for combination array
+		int i = 0; // index for elements array
+		
+		while(r >= 0){
+		
+			// forward step if i < (N + (r-K))
+			if(i <= (N + (r - K))){
+					combination[r] = i;
+					
+				// if combination array is full print and increment i;
+				if(r == K-1){
+					print(combination, elements);
+					i++;				
+				}
+				else{
+					// if combination is not full yet, select next element
+					i = combination[r]+1;
+					r++;										
+				}
+				
+			}
+			
+			// backward step
+			else{
+				r--;
+				if(r >= 0)
+					i = combination[r]+1;
+				
+			}			
+		}
+	}
+	
+
+	
+	private static int c(int n, int r){
+		int nf=fact(n);
+		int rf=fact(r);
+		int nrf=fact(n-r);
+		int npr=nf/nrf;
+		int ncr=npr/rf; 
+		
+		System.out.println("C("+n+","+r+") = "+ ncr);
+
+		return ncr;
+	}
+	
+	private static int fact(int n)
+	{
+		if(n == 0)
+			return 1;
+		else
+			return n * fact(n-1);
+	}
+	
+
+	public int getNumberOfWays(final int total, final int k) {
+		String output = "";
+		for(int z = 0 ; z < combination.length;z++){
+			output += elements[combination[z]];
+		}
+		System.out.println(output);
+	}
 }
